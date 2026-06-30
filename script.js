4b26976892f5b99d1157dce9c25a7d662095d4d1
ajmolhosain060-web <ajmolhosain060@gmail.com>
2026-06-30 18:18:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-// Moharani
+const loader = document.getElementById("loader");
+
+window.addEventListener("load", () => {
+
+    setTimeout(() => {
+
+        loader.style.opacity = "0";
+
+        loader.style.transition = "1s";
+
+        setTimeout(() => {
+
+            loader.style.display = "none";
+
+        },1000);
+
+    },3000);
+
+});
+
+
+const btn = document.getElementById("startBtn");
+
+btn.addEventListener("mouseenter",()=>{
+
+    btn.style.transform="scale(1.08)";
+
+});
+
+btn.addEventListener("mouseleave",()=>{
+
+    btn.style.transform="scale(1)";
+
+});
+
+
+btn.addEventListener("click",()=>{
+
+    alert("🌸 The journey has just begun, Moharani ❤️");
+
+});
